409b5630419759aa5e2e07393c2db32d271ee34e
vinoankr <vinoankr@gmail.com>
2024-03-12 17:49:23 +0900

    auto update the current index of timetable

--- src/components/Timetable/Timetable.jsx
+++ src/components/Timetable/Timetable.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+import useAutoReload from "../../hooks/useAutoReload";
 import { getCurrentTimetableIndex } from "../../utils/time";
 import TimetableItem from "./TimetableItem";
 import * as S from "./TimetableStyled";
 
 export default function Timetable({ table, classPA }) {
-  const currentIndex = getCurrentTimetableIndex();
+  const [currentIndex, setCurrentIndex] = useState(null);
+
+  function getTimetableIndex() {
+    const newIndex = getCurrentTimetableIndex();
+    setCurrentIndex(newIndex);
+  }
+
+  useEffect(() => getTimetableIndex(), []);
+  useAutoReload(getTimetableIndex);
+
   if (!table || table.length !== 22) {
     return <div>insufficient timetable data</div>;
   }
