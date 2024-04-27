c610116e53b95fb4bf8baf9546179cd1e23f8a19
vinoankr <vinoankr@gmail.com>
2024-04-27 16:02:00 +0900

    fix modal black space with virtual keyboard

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+// import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 export const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,6 +11,7 @@ export const useModal = () => {
     root.style.overflow = "hidden";
     root.style.pointerEvents = "none";
     root.style.position = "fixed";
+    root.style.height = "100%";
     root.style.top = `-${scrollPosition}px`;
     root.style.left = "0";
     root.style.right = "0";
@@ -20,6 +22,7 @@ export const useModal = () => {
     root.style.removeProperty("overflow");
     root.style.removeProperty("pointer-events");
     root.style.removeProperty("position");
+    root.style.removeProperty("height");
     root.style.removeProperty("top");
     root.style.removeProperty("left");
     root.style.removeProperty("right");
