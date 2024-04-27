93308413bdc1b418758828fa66f5c77bd30bab24
vinoankr <vinoankr@gmail.com>
2024-04-27 15:47:30 +0900

    fix and test

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -2,27 +2,27 @@ import { useState } from "react";
 
 export const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const body = document.body;
+  const root = document.getElementById("root") as HTMLElement;
   const scrollPosition = window.scrollY;
 
   const handleModalOpen = () => {
     setIsModalOpen(true);
-    body.style.overflow = "hidden";
-    body.style.pointerEvents = "none";
-    body.style.position = "fixed";
-    body.style.top = `-${scrollPosition}px`;
-    body.style.left = "0";
-    body.style.right = "0";
+    root.style.overflow = "hidden";
+    root.style.pointerEvents = "none";
+    root.style.position = "fixed";
+    root.style.top = `-${scrollPosition}px`;
+    root.style.left = "0";
+    root.style.right = "0";
   };
 
   const handleModalClose = () => {
     setIsModalOpen(false);
-    body.style.removeProperty("overflow");
-    body.style.removeProperty("pointer-events");
-    body.style.removeProperty("position");
-    body.style.removeProperty("top");
-    body.style.removeProperty("left");
-    body.style.removeProperty("right");
+    root.style.removeProperty("overflow");
+    root.style.removeProperty("pointer-events");
+    root.style.removeProperty("position");
+    root.style.removeProperty("top");
+    root.style.removeProperty("left");
+    root.style.removeProperty("right");
     window.scrollTo(0, scrollPosition);
   };
 
