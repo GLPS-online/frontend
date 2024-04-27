139691d03b4864720662ab3c73097844e312aeeb
vinoankr <vinoankr@gmail.com>
2024-04-27 20:42:24 +0900

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 
 const client = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
