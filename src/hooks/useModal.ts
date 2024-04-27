d1167bb7fcc87c1e66d96dde921d67d64ecb76be
vinoankr <vinoankr@gmail.com>
2024-04-27 15:29:42 +0900

    fix iphone bug

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -6,13 +6,11 @@ export const useModal = () => {
   const handleModalOpen = () => {
     setIsModalOpen(true);
     document.body.style.overflow = "hidden";
-    document.body.style.pointerEvents = "none";
   };
 
   const handleModalClose = () => {
     setIsModalOpen(false);
     document.body.style.overflow = "auto";
-    document.body.style.removeProperty("pointer-events");
   };
 
   return {
