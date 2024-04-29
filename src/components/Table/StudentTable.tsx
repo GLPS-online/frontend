e4b01a41f917e7d4f8afb3f557bf9403c9e5d278
vinoan <vinoan@umich.edu>
2024-04-29 21:43:10 +0900

    fix student table selection botteneck

--- src/components/Table/StudentTable.tsx
+++ src/components/Table/StudentTable.tsx
@@ -237,7 +237,7 @@ export default function StudentTable({ data }: Props) {
         }
 
         {filteredData.map((student: Student) => (
-          <S.RowContainer key={`${rerenderer}${student._id}`}>
+          <S.RowContainer key={`${student._id}${selectedItems.has(student._id)?"1":""}`}>
             <S.CheckBox
               name={student._id}
               type={action !== "default" ? "checkbox" : "hidden"}
