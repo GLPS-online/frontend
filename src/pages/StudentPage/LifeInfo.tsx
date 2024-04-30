ec0035e78e429fe0be8ab36abfb05bd379c3a4ed
vinoan <vinoan@umich.edu>
2024-04-30 20:43:52 +0900

    remove unused imports

--- src/pages/StudentPage/LifeInfo.tsx
+++ src/pages/StudentPage/LifeInfo.tsx
@@ -1,11 +1,10 @@
 import Student from "@/interfaces/Student";
-import * as S from "./StudentPageStyled";
 
 export default function LifeInfo({ student }: { student: Student }) {
   return (
-    <S.InfoContainer>
+    <div>
       <div>상담 기록</div>
       <div>-- 삭제예정 --</div>
-    </S.InfoContainer>
+    </div>
   );
 }
