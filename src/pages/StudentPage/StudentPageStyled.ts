f463c39fe71e2ca50714402d6ac2a495ee3bc737
vinoan <vinoan@umich.edu>
2024-04-30 20:42:14 +0900

    Remove unused lines

--- src/pages/StudentPage/StudentPageStyled.ts
+++ src/pages/StudentPage/StudentPageStyled.ts
@@ -7,20 +7,3 @@ export const PageContainer = styled.div`
   align-items: flex-start;
   gap: 20px;
 `;
-
-export const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 10px;
-`;
-
-export const EditSave = styled.img`
-  width: 16px;
-  height: 16px;
-`;
-
-export const Button = styled.button`
-  width: 16px;
-  height: 16px;
-`;
